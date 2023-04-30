b7f6dbd52b43f1ab7c59b8b1d340f6709c5c1317
monir3355 <16173103036@cse.bubt.edu.bd>
2023-04-30 18:58:40 +0600

    Categories and news added with spacific id

--- index.js
+++ index.js
@@ -3,16 +3,39 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+
 app.use(cors());
 
-const categories = require("./data/categories.json");
 app.get("/", (req, res) => {
-  res.send("The Orange News Is Running");
+  res.send("Dragon is running");
 });
+
 app.get("/categories", (req, res) => {
   res.send(categories);
 });
 
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Dragon API is running on port: ${port}`);
 });
